140ccf0be5ef5b18a0c2250e0d3b6e599a248c4d
Dominic <DominicTheDev89@gmail.com>
2023-02-17 12:03:09 -0500

    Object Practice

---
+++ car-trip.js
@@ -0,0 +1,27 @@
+// Write a function that takes in a car object (in the form `{people, miles, spareTiresUsed}`) and a number of miles driven as an integer. 
+// Update the car object to include the new amount of miles on the odometer. Additionally, the road is very rocky, so you'll need 
+// to use an additional spare tire every 1000 miles. Update the car object to reflect this, too.
+
+// Example
+
+// carTrip({
+//     people: ["Spiderman", "Batman"],
+//     miles: 1500,
+//     spareTiresUsed: 0
+// }, 5500)
+
+// =>
+
+// {
+//     people: ["Spiderman", "Batman"],
+//     miles: 7000,
+//     spareTiresUsed: 5
+// } 
+
+//My Solution
+
+function carTrip(car, miles) {
+    car['miles'] += miles;
+    car['spareTiresUsed'] = car['spareTiresUsed'] + Math.floor(miles/1000);
+    return car;
+  }
