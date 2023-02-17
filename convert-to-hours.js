463aa503a024259da990ca9da292efee213f60bd
Dominic <DominicTheDev89@gmail.com>
2023-02-16 19:24:44 -0500

    Another coding challenge

---
+++ convert-to-hours.js
@@ -0,0 +1,6 @@
+// Write a function that takes in a number of minutes and returns the hour(s) equivalent as a float integer.
+
+//My Solution
+function toHours(minutes) {
+    return (minutes/60).toFixed(2)
+  }
