7ab5a516ece6d84c0391f8de575e240265b4cb4c
Dominic <DominicTheDev89@gmail.com>
2023-02-19 10:42:39 -0500

    Working without using loops practice

---
+++ no-loops-allowed.js
@@ -0,0 +1,20 @@
+// *** No Loops Allowed ***
+
+// You will be given an array (a) and a limit value (limit). 
+// You must check that all values in the array are below or equal to the limit value. 
+// If they are, return true. Else, return false.
+
+// You can assume all values in the array are numbers.
+
+// Do not use loops. Do not modify input array.
+
+// My solution
+
+function smallEnough(a, limit){
+    let newArr = a.filter(num => num <= limit)
+    if(newArr.length == a.length){
+      return true;
+    } else {
+      return false;
+    }
+  }
