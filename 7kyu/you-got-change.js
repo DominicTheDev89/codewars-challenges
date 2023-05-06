6cd844ce1d93ee4161838e048ed4715e3ee3174b
Dominic <DominicTheDev89@gmail.com>
2023-05-06 15:47:00 -0400

    Daily coding challenge

---
+++ 7kyu/you-got-change.js
@@ -0,0 +1,73 @@
+// Create a function that will take any amount of money and break it down to the smallest number of bills as 
+// possible. Only integer amounts will be input, NO floats. This function should output a sequence, where each element in the 
+// array represents the amount of a certain bill type. The array will be set up in this manner:
+
+// array[0] ---> represents $1 bills
+
+// array[1] ---> represents $5 bills
+
+// array[2] ---> represents $10 bills
+
+// array[3] ---> represents $20 bills
+
+// array[4] ---> represents $50 bills
+
+// array[5] ---> represents $100 bills
+
+// In the case below, we broke up $365 into 1 $5 bill, 1 $10 bill, 1 $50 bill, and 3 $100 bills:
+
+// 365 =>  [0,1,1,0,1,3]
+// In this next case, we broke $217 into 2 $1 bills, 1 $5 bill, 1 $10 bill, and 2 $100 bills:
+
+// 217 => [2,1,1,0,0,2]
+
+// My solution
+
+function giveChange(amount) {
+    let answer = [0,0,0,0,0,0]
+    let i = amount;
+    if(i >= 100){
+      while(i >= 100){
+        i -= 100
+        answer[5]++
+      }
+    }
+    
+    if(i >= 50){
+      while(i >= 50){
+        i -= 50
+        answer[4]++
+      }
+    }
+    
+    if(i >= 20){
+      while(i >= 20){
+        i -= 20
+        answer[3]++
+      }
+    }
+    
+    if(i >= 10){
+      while(i >= 10){
+        i -= 10
+        answer[2]++
+      }
+    }
+    
+    if(i >= 5){
+      while(i >= 5){
+        i -= 5
+        answer[1]++
+      }
+    }
+    
+    if(i >= 1){
+      while(i >= 1){
+        i -= 1
+        answer[0]++
+      }
+    }
+    
+    return answer;
+    
+  }
